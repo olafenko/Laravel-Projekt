06c56cf8ba9366949770ebb7a5324677bb5eaf34
olafenko <olopikolo666@gmail.com>
2026-06-15 19:56:54 +0200

    feature/ add middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,22 @@ use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ListingController::class,"index"]);
+Route::get('/auth/login',[AuthController::class,"loginView"])->name("login")->middleware("guest");
+Route::post('/auth/login',[AuthController::class,"login"])->middleware("guest");
+
+Route::post('/auth/logout',[AuthController::class,"logout"])->middleware("guest");
+
+Route::get('/auth/register',[AuthController::class,"registerView"])->middleware("guest");
+Route::post('/auth/register',[AuthController::class,"register"])->middleware("guest");
+
 Route::get('/listings', [ListingController::class,"index"]);
 
-Route::get('/auth/login',[AuthController::class,"loginView"]);
-Route::post('/auth/login',[AuthController::class,"login"]);
-Route::post('/auth/logout',[AuthController::class,"logout"]);
+Route::get('/listings/create',[ListingController::class,"listingForm"])->middleware("auth");
+Route::post('/listings/create',[ListingController::class,"create"])->middleware("auth");;
 
-Route::get('/auth/register',[AuthController::class,"registerView"]);
-Route::post('/auth/register',[AuthController::class,"register"]);
+Route::get('/listings/edit/{id}',[ListingController::class,"listingForm"])->middleware("auth");;
+Route::post('/listings/edit/{id}',[ListingController::class,"edit"])->middleware("auth");;
 
-Route::get('/listings/create',[ListingController::class,"listingForm"]);
-Route::post('/listings/create',[ListingController::class,"create"]);
-
-Route::get('/listings/edit/{id}',[ListingController::class,"listingForm"]);
-Route::post('/listings/edit/{id}',[ListingController::class,"edit"]);
-
-Route::post('/listings/delete/{id}',[ListingController::class,"delete"]);
+Route::post('/listings/delete/{id}',[ListingController::class,"delete"])->middleware("auth");;
 
 
