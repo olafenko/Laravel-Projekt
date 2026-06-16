4800201e2d1614eab2b9f20440dfcfcdfe9d4cf0
olafenko <olopikolo666@gmail.com>
2026-06-16 21:19:42 +0200

    apperaence/ add simple footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,9 @@
     @yield('content')
 </main>
 
-
+<footer>
+    <p>&copy 2026 Serwis ogłoszeniowy - Olaf Chechelski</p>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
